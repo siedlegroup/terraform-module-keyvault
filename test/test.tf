9270df9fa3c2dbd8d6dcb0cc167a16a0ddcd877d
Hakan Kaya <80452889+kayahk@users.noreply.github.com>
2024-03-15 15:53:09 +0100

    changed provider version constraint

--- test/test.tf
+++ test/test.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.96.0, < 4.0.0"
+      version = "~> 3.89.0, < 4.0.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
